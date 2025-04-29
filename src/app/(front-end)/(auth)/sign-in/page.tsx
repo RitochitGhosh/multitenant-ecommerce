9d5a3cea06c8e78db9c445262a88909f2936e4b8
RitochitGhosh <ghosh.18.ritochit12@gmail.com>
2025-04-29 17:45:13 +0530

    08: Authentication

---
+++ src/app/(front-end)/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view"
+
+const Page = () => {
+    return (
+        <div className="">
+            <SignInView />
+        </div>
+    )
+}
+
+export default Page
--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,16 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  admin: {
+    useAsTitle: 'email',
+  },
   auth: true,
   fields: [
+    {
+      name: "username",
+      type: "text",
+      required: true,
+      unique: true,
+    },
   ],
 }
